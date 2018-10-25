ef12bf1b5fa1d5cdf4ab8615db9d8ac7a1bfd463
Leron <mkubacki@uni-bielefeld.de>
2018-10-25 10:59:37 +0200

    Update HIWI-IBM.R

--- HIWI-IBM.R
+++ HIWI-IBM.R
@@ -3,7 +3,7 @@
 #requirements
 # migration, multiple patches(the amount can be changed via 1 variable)
 # male/female,Age, a traitvalue
-
+rm(list=ls())
 
 #Names will be changed, i´ve just used the first Names that came to mind
 patches<-5
@@ -12,13 +12,13 @@ patches<-5
     Apollo<-round(rnorm(1, mean=50, sd=10))
     Artemis<-round(rnorm(1, mean=50, sd=10))
       Hyakinthos<-Apollo+Artemis-1
-    Leto<-rbind(rep("female",Artemis),rep("male",Apollo)) #What am i doing wrong here?
+    Leto<-rbind(matrix(rep("female",Artemis),ncol=1),matrix(rep("male",Apollo),ncol=1))
     Nike<-cbind(round(runif(1,1,5)),round(runif(1,1,6)))
     for(z in 1:Hyakinthos){
       Nike<-rbind(Nike,cbind(round(runif(1,1,5)),round(runif(1,1,6))))
     }
     Nike
     Leto<-cbind(Leto,Nike)
-    
+    colnames(Leto)<-c("Gender","Age","Trait")
   }
-Patch1<-c("Gender","Age","Trait")
+
