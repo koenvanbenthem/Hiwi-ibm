ed5fcb05b6bb0ba704ef8c2f11e3976964b701b7
Pilence13 <hannah.zivkovic@web.de>
2018-10-25 11:31:23 +0200

    Update HIWI-IBM.R

--- HIWI-IBM.R
+++ HIWI-IBM.R
@@ -5,27 +5,7 @@
 # male/female,Age, a traitvalue
 rm(list=ls())
 
-<<<<<<< HEAD
-#Names will be changed, i´ve just used the first Names that came to mind
-patches<-5
-  
-  for(i in 1:patches){
-    Apollo<-round(rnorm(1, mean=50, sd=10))
-    Artemis<-round(rnorm(1, mean=50, sd=10))
-      Hyakinthos<-Apollo+Artemis-1
-    Leto<-rbind(matrix(rep("female",Artemis),ncol=1),matrix(rep("male",Apollo),ncol=1))
-    Nike<-cbind(round(runif(1,1,5)),round(runif(1,1,6)))
-    for(z in 1:Hyakinthos){
-      Nike<-rbind(Nike,cbind(round(runif(1,1,5)),round(runif(1,1,6))))
-    }
-    Nike
-    Leto<-cbind(Leto,Nike)
-    colnames(Leto)<-c("Gender","Age","Trait")
-  }
-=======
-#Names will be changed, iÂ´ve just used the first Names that came to mind
-
->>>>>>> hannah
+#Names will be changed, iï¿½ve just used the first Names that came to mind
 
 ##### DEFINED PARAMETERS #####
 
